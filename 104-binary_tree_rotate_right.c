2790dc38f1135a38f139ecee4758398c95b1e6ee
Astambar <leuchartjason@gmail.com>
2022-07-20 13:47:42 +0000

    104 advanced task

---
+++ 104-binary_tree_rotate_right.c
@@ -0,0 +1,12 @@
+/**
+
+* binary_tree_rotate_right - function that performs a right-rotation on a binary tree
+* @tree: is a pointer to the root node of the tree to rotate
+* ======================================================================
+* Your function must return a pointer to the 
+* new root node of the tree once rotated
+*/
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
+{
+
+}
