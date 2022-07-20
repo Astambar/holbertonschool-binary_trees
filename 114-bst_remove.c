8c2f2435e4d44dea96842a45866fbe9f851515c7
Astambar <leuchartjason@gmail.com>
2022-07-20 13:58:46 +0000

    114 advanced task

---
+++ 114-bst_remove.c
@@ -0,0 +1,13 @@
+/**
+
+bst_remove - function that removes a node from a Binary Search Tree
+    Where root is a pointer to the root node of the tree where you will remove a node
+    And value is the value to remove in the tree
+    Once located, the node containing a value equals to value must be removed and freed
+    If the node to be deleted has two children, it must be replaced with its first in-order successor (not predecessor)
+    Your function must return a pointer to the new root node of the tree after removing the desired value
+*/
+bst_t *bst_remove(bst_t *root, int value)
+{
+
+}
