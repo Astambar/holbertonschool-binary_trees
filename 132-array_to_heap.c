ed511cc7176d348b60d0444d7df7494335fdec82
Astambar <leuchartjason@gmail.com>
2022-07-20 14:16:06 +0000

    132 advanced task

---
+++ 132-array_to_heap.c
@@ -0,0 +1,10 @@
+/**
+array_to_heap - function that builds a Max Binary Heap tree from an array
+    Where array is a pointer to the first element of the array to be converted
+    And size is the number of element in the array
+    Your function must return a pointer to the root node of the created Binary Heap, or NULL on failure
+*/
+heap_t *array_to_heap(int *array, size_t size)
+{
+
+}
