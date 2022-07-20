ca957a494451edf032c7a2cf21f31dcb2aef3df7
Astambar <leuchartjason@gmail.com>
2022-07-20 14:03:27 +0000

    122 advanced task

---
+++ 122-array_to_avl.c
@@ -0,0 +1,12 @@
+/**
+
+array_to_avl - function that builds an AVL tree from an array
+    Where array is a pointer to the first element of the array to be converted
+    And size is the number of element in the array
+    Your function must return a pointer to the root node of the created AVL tree, or NULL on failure
+    If a value of the array is already present in the tree, this value must be ignored
+*/
+avl_t *array_to_avl(int *array, size_t size)
+{
+
+}
