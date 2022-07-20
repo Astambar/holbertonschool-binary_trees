192f934f57cae5bcf7749446e769d98e9723c074
Astambar <leuchartjason@gmail.com>
2022-07-20 13:53:50 +0000

    110 advanced task

---
+++ 110-binary_tree_is_bst.c
@@ -0,0 +1,12 @@
+/**
+* binary_tree_is_bst - function that checks if a binary
+* tree is a valid Binary Search Tree
+* @tree: is a pointer to the root node of the tree to check
+* =====================================================================
+*    Your function must return 1 if tree is a valid BST, and 0 otherwise
+*    If tree is NULL, return 0
+*/
+int binary_tree_is_bst(const binary_tree_t *tree)
+{
+
+}
