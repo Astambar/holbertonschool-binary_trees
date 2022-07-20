1e6e202e3cc3714800775e47d74e71a7ca5fb7c8
Astambar <leuchartjason@gmail.com>
2022-07-20 14:15:13 +0000

    131 advanced task

---
+++ 131-heap_insert.c
@@ -0,0 +1,13 @@
+/**
+heap_insert - function that converts a Binary Max Heap to a sorted array of integers
+    Where root is a double pointer to the root node of the Heap to insert the value
+    And value is the value to store in the node to be inserted
+    Your function must return a pointer to the created node, or NULL on failure
+    If the address stored in root is NULL, the created node must become the root node.
+    You have to respect a Max Heap ordering
+    You are allowed to have up to 6 functions in your file
+*/
+heap_t *heap_insert(heap_t **root, int value)
+{
+
+}
