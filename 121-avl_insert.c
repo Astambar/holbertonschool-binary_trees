1797f44418a35806cb0a6a2122f95f2f9a1c7211
Astambar <leuchartjason@gmail.com>
2022-07-20 14:02:42 +0000

    121 advanced task

---
+++ 121-avl_insert.c
@@ -0,0 +1,13 @@
+/**
+
+avl_insert - function that inserts a value in an AVL Tree
+    Where tree is a double pointer to the root node of the AVL tree for inserting the value
+    And value is the value to store in the node to be inserted
+    Your function must return a pointer to the created node, or NULL on failure
+    If the address stored in tree is NULL, the created node must become the root node.
+    The resulting tree after insertion, must be a balanced AVL Tree
+*/
+avl_t *avl_insert(avl_t **tree, int value)
+{
+
+}
