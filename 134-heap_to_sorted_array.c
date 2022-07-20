d849378ef4342be19edcf9f7e3492e4e6998ef7a
Astambar <leuchartjason@gmail.com>
2022-07-20 14:18:40 +0000

    134 advanced task

---
+++ 134-heap_to_sorted_array.c
@@ -0,0 +1,8 @@
+/**
+
+    Prototype: int *heap_to_sorted_array(heap_t *heap, size_t *size);
+    Where heap is a pointer to the root node of the heap to convert
+    And size is an address to store the size of the array
+    You can assume size is a valid address
+    Since we are using Max Heap, the returned array must be sorted in descending order
+*/
