abb7d5ac3b98731e3496bd37d6f2ded1c3f64162
Astambar <leuchartjason@gmail.com>
2022-07-20 14:01:39 +0000

    120 advanced task

---
+++ 120-binary_tree_is_avl.c
@@ -0,0 +1,11 @@
+/**
+
+binary_tree_is_avl - function that checks if a binary tree is a valid AVL Tree
+    Where tree is a pointer to the root node of the tree to check
+    Your function must return 1 if tree is a valid AVL Tree, and 0 otherwise
+    If tree is NULL, return 0
+*/
+int binary_tree_is_avl(const binary_tree_t *tree)
+{
+
+}
