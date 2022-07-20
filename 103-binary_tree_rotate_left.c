3e4152ce43275979dcd37b0804826f12cfe27b63
Astambar <leuchartjason@gmail.com>
2022-07-20 13:46:54 +0000

    103 advanced task

---
+++ 103-binary_tree_rotate_left.c
@@ -0,0 +1,11 @@
+/**
+*
+* binary_tree_t - function that performs a left-rotation on a binary tree
+* @tree: is a pointer to the root node of the tree to rotate
+* =======================================================================
+* Your function must return a pointer to the new root node of the tree once rotated
+*/
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
+{
+
+}
