1a96c1e44ed6deb04ef599e73856814bb589626c
Astambar <leuchartjason@gmail.com>
2022-07-20 14:04:08 +0000

    123 advanced task

---
+++ 123-avl_remove.c
@@ -0,0 +1,14 @@
+/**
+
+avl_remove - function that removes a node from an AVL tree
+    Where root is a pointer to the root node of the tree for removing a node
+    And value is the value to remove in the tree
+    Once located, the node containing a value equals to value must be removed and freed
+    If the node to be deleted has two children, it must be replaced with its first in-order successor (not predecessor)
+    After deletion of the desired node, the tree must be rebalanced if necessary
+    Your function must return a pointer to the new root node of the tree after removing the desired value, and after rebalancing
+*/
+avl_t *avl_remove(avl_t *root, int value)
+{
+
+}
