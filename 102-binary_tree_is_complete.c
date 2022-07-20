5b206df6e68fe3b86efce6abb4b627137b773995
Astambar <leuchartjason@gmail.com>
2022-07-20 13:46:01 +0000

    102 advanced task

---
+++ 102-binary_tree_is_complete.c
@@ -0,0 +1,11 @@
+/**
+* binary_tree_levelorder - function that checks
+* if a binary tree is complete
+* @tree:  is a pointer to the root node of the tree to check
+* ==========================================
+* If tree is NULL, your function must return 0
+*/
+int binary_tree_is_complete(const binary_tree_t *tree)
+{
+
+}
