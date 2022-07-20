53a35a7d90da95007e2c4c16b7a6d1eea904587a
Astambar <leuchartjason@gmail.com>
2022-07-20 13:56:25 +0000

    111 advanced task

---
+++ 111-bst_insert.c
@@ -0,0 +1,15 @@
+/**
+* 
+bst_insert - Function that inserts a value in a Binary Search Tree
+@tree: is a double pointer to the root node of the BST to insert the value
+@value: is the value to store in the node to be inserted
+    Your function must return a pointer to the created node, or NULL on failure
+    If the address stored in tree is NULL, the created node must become the root node.
+    If the value is already present in the tree, it must be ignored
+
+*
+*/
+bst_t *bst_insert(bst_t **tree, int value)
+{
+
+}
